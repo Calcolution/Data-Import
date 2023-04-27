f2669787e2bd30da9767de0d7d308702dde50a45
Ujjwal Khadka <ujjwal.khadka@consileon.de>
2023-04-27 15:52:26 +0200

    updated file to accept input from user

--- ImportExcelToSQL.py
+++ ImportExcelToSQL.py
@@ -2,13 +2,14 @@ import pandas as pd
 import pyodbc
 from openpyxl import load_workbook
 
-# Replace these values with your own credentials and file path
-server = 'your_server.database.windows.net' # 'calcolution1.database.windows.net'
-database = 'your_database' # 'calcolution'
-username = 'your_username' # 'calcolution'
-password = 'your_password' # '123*testinG'
-driver = '{ODBC Driver 17 for SQL Server}' #
-excel_file_path = 'your_excel_file.xlsx' # 'Datenmodell_4-2304-Qual-and-Quant-Dataprovider.xlsx'
+def get_user_input():
+    server = input("Enter server: ").strip()
+    database = input("Enter database: ").strip()
+    username = input("Enter username: ").strip()
+    password = input("Enter password: ").strip()
+    excel_file_path = input("Enter Excel file path: ").strip()
+
+    return server, database, username, password, excel_file_path
 
 def create_table(cursor, table_name, columns):
     create_query = f"CREATE TABLE {table_name} (" + ", ".join(
@@ -22,45 +23,52 @@ def insert_data(cursor, table_name, df):
     cursor.fast_executemany = True
     cursor.executemany(insert_query, df.fillna('').astype(str).values.tolist())
 
-try:
-    # Connect to Azure SQL Database
-    cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-    cursor = cnxn.cursor()
+server, database, username, password, excel_file_path = get_user_input()
 
-    # Load Excel workbook
-    workbook = load_workbook(excel_file_path)
-    sheet_names = workbook.sheetnames
+if server and database and username and password and excel_file_path:
+    try:
+        # Connect to Azure SQL Database
+        driver = '{ODBC Driver 18 for SQL Server}'
+        cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+        cursor = cnxn.cursor()
 
-    # Import each worksheet into Azure SQL
-    for sheet_name in sheet_names:
-        # Read worksheet into a Pandas DataFrame
-        print(f"Trying to create table{sheet_name}")
-        df = pd.read_excel(excel_file_path, sheet_name=sheet_name, engine='openpyxl')
+        # Load Excel workbook
+        workbook = load_workbook(excel_file_path)
+        sheet_names = workbook.sheetnames
 
-        # Replace spaces in column names with underscores
-        df.columns = [col.replace(' ', '_') for col in df.columns]
+        # Import each worksheet into Azure SQL
+        for sheet_name in sheet_names:
+            # Read worksheet into a Pandas DataFrame
+            print(f"Trying to create table '{sheet_name}'")
+            df = pd.read_excel(excel_file_path, sheet_name=sheet_name, engine='openpyxl')
 
-        # Create table in Azure SQL based on DataFrame
-        table_name = f"{sheet_name.replace(' ', '_')}_data"
-        try:
-            create_table(cursor, table_name, df.columns)
-            cnxn.commit()
-        except pyodbc.Error as e:
-            print(f"Error creating table '{table_name}': {e}")
-            continue
+            # Replace spaces in column names with underscores
+            df.columns = [col.replace(' ', '_') for col in df.columns]
 
-        # Insert DataFrame data into the table
-        try:
-            insert_data(cursor, table_name, df)
-            cnxn.commit()
-        except pyodbc.Error as e:
-            print(f"Error inserting data into '{table_name}': {e}")
-            continue
+            # Create table in Azure SQL based on DataFrame
+            table_name = f"{sheet_name.replace(' ', '_')}_data"
+            try:
+                create_table(cursor, table_name, df.columns)
+                cnxn.commit()
+            except pyodbc.Error as e:
+                print(f"Error creating table '{table_name}': {e}")
+                continue
 
-except pyodbc.Error as e:
-    print(f"Error connecting to Azure SQL Database: {e}")
+            # Insert DataFrame data into the table
+            try:
+                insert_data(cursor, table_name, df)
+                cnxn.commit()
+                print(f"Inserted data to table '{sheet_name}'")
+            except pyodbc.Error as e:
+                print(f"Error inserting data into '{table_name}': {e}")
+                continue
 
-finally:
-    # Close database connection
-    cursor.close()
-    cnxn.close()
+    except pyodbc.Error as e:
+        print(f"Error connecting to Azure SQL Database: {e}")
+
+    finally:
+        # Close database connection
+        cursor.close()
+        cnxn.close()
+else:
+    print("Please provide all the required details.")
--- README.md
+++ README.md
@@ -1,44 +1,38 @@
-# Data-Import
+# Import Excel to Azure SQL Python Script
 
-Here's a step-by-step guide to help you install the required packages, set up the environment, and run the code to import Excel data into Azure SQL.
+This Python script imports data from an Excel file into an Azure SQL database. The script reads each worksheet in the Excel file, creates a table for each worksheet, and imports the data into the corresponding table.
 
-### Step 1: Install Python
-1. Download and install Python 3 from the official website: https://www.python.org/downloads/
-2. Make sure to check the "Add Python to PATH" option during installation.
-3. Open a terminal or command prompt and run `python --version` to check the successful installation. You should see the version of Python you just installed.
+## Prerequisites
 
-### Step 2: Install Required Packages
-1. Open a terminal or command prompt and run the following command to install the necessary packages:
+- Python 3.x
+- Required packages: `pandas`, `pyodbc`, `openpyxl`
+- ODBC Driver 17 or 18 for SQL Server
+- An Azure SQL database
+
+## Instructions
+
+1. Make sure you have installed the required packages by running the following command:
+
    ```
    pip install pandas pyodbc openpyxl
    ```
 
-### Step 3: Set up an Azure SQL Database
-1. Sign in to the Azure portal: https://portal.azure.com/
-2. Create an Azure SQL Database by following the official documentation: https://docs.microsoft.com/en-us/azure/azure-sql/database/single-database-create-quickstart?tabs=azure-portal
-3. Replace the following variables in the Python code with your own Azure SQL Database connection details:
+2. Check if you have ODBC Driver 17 or 18 for SQL Server installed. If you have version 18, keep the driver variable as is. If you have version 17, change the driver variable in the code to:
+
    ```python
-   server = 'your_server.database.windows.net'
-   database = 'your_database'
-   username = 'your_username'
-   password = 'your_password'
    driver = '{ODBC Driver 17 for SQL Server}'
    ```
 
-### Step 4: Install ODBC Driver for SQL Server
-1. Download and install the ODBC Driver for SQL Server from the official website: https://docs.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver15
-2. Make sure to choose the appropriate driver version (x86 or x64) based on your system architecture.
+3. Make sure not to use spaces in the Excel file name, as this may cause issues when creating tables in the Azure SQL database. It's best to choose a file name without spaces.
 
-### Step 5: Prepare the Excel File
-1. Make sure the Excel file you want to import into Azure SQL is in the `.xlsx` format and each worksheet has a header row with column names.
-2. Replace the `excel_file_path` variable in the Python code with the path to your Excel file:
-   ```python
-   excel_file_path = 'your_excel_file.xlsx'
-   ```
+4. Make sure to provide the correct path to the Excel file in the `excel_file_path` variable. To avoid confusion, it's recommended to keep the Excel file in the same folder as the Python script.
 
-### Step 6: Run the Python Script
-1. Open a terminal or command prompt, navigate to the folder containing the script, and run the following command:
-   ```
-   python ImportExcelToSQL.py
-   ```
-2. The script will create a table for each worksheet in the Excel file and import the data into Azure SQL. If there are any errors, the script will print them to the console.
+5. Ensure your IP is added to the Azure SQL Server firewall rules to allow access to the database.
+
+6. Run the Python script. The script will create a table for each worksheet in the Excel file and import the data into the corresponding table in the Azure SQL database. If there are any errors, the script will print them to the console.
+
+## Notes
+
+- The script assumes that each worksheet in the Excel file has a header row with column names.
+- The script replaces spaces in column names with underscores.
+- The script handles connection errors and issues when creating tables and inserting data into the tables.
